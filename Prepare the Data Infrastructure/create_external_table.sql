ea8b4f8316a8f7ae9b9fb11480f62bf176ea3aa6
Halah Abousohh <eng.halah.mohammed212@outlook.sa>
2025-12-04 07:24:56 +0100

    Refactor SQL script for external table creation

--- Prepare the Data Infrastructure/create_external_table.sql
+++ Prepare the Data Infrastructure/create_external_table.sql
@@ -1,37 +1,37 @@
-use dbo;
+-- Create the External File Format
 IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name = 'SynapseDelimitedTextFormat') 
-	CREATE EXTERNAL FILE FORMAT [SynapseDelimitedTextFormat] 
-	WITH ( FORMAT_TYPE = DELIMITEDTEXT ,
-	       FORMAT_OPTIONS (
-			 FIELD_TERMINATOR = ',',
-			 USE_TYPE_DEFAULT = FALSE
-			))
+CREATE EXTERNAL FILE FORMAT [SynapseDelimitedTextFormat] 
+WITH ( 
+    FORMAT_TYPE = DELIMITEDTEXT,
+    FORMAT_OPTIONS (
+        FIELD_TERMINATOR = ',',
+        USE_TYPE_DEFAULT = FALSE
+    )
+)
 GO
 
-IF NOT EXISTS (SELECT * FROM sys.external_data_sources WHERE name = 'synapsefilesystem_synapsedldemo_dfs_core_windows_net') 
-	CREATE EXTERNAL DATA SOURCE [synapsefilesystem_synapsedldemo_dfs_core_windows_net] 
-	WITH (
-		LOCATION ='abfss://synapsefilesystem@synapsedldemo.dfs.core.windows.net'  
-	)
+-- Create the External Data Source WITHOUT credential (for public access or managed identity)
+IF NOT EXISTS (SELECT * FROM sys.external_data_sources WHERE name = 'defs_de2_dfs_core_windows_net')
+CREATE EXTERNAL DATA SOURCE [defs_de2_dfs_core_windows_net]
+WITH (
+    LOCATION = 'abfss://defs@de2.dfs.core.windows.net'
+    -- No credential needed if using public access or Synapse workspace managed identity
+)
 GO
 
-
-
-
-CREATE EXTERNAL TABLE [dbo].[NYC_Payroll_Summary] (
-	[FiscalYear] [int] NULL,
-	[AgencyName] [varchar](50) NULL,
-	[TotalPaid] [float] NULL
-
-	)
-	WITH (
-	LOCATION = '/',
-	DATA_SOURCE = [synapsefilesystem_synapsedldemo_dfs_core_windows_net],
-	FILE_FORMAT = [SynapseDelimitedTextFormat]
-	)
+-- Create the External Table
+CREATE EXTERNAL TABLE [dbo].[NYC_Payroll_Summary](
+    [FiscalYear] [int] NULL,
+    [AgencyName] [varchar](50) NULL,
+    [TotalPaid] [float] NULL
+)
+WITH (
+    LOCATION = '/',
+    DATA_SOURCE = [defs_de2_dfs_core_windows_net],
+    FILE_FORMAT = [SynapseDelimitedTextFormat]
+)
 GO
 
-
-
-SELECT TOP 100 * FROM [dbo].[NYC_Payroll_Summary]
+-- Check if it was created successfully
+SELECT TOP 10 * FROM [dbo].[NYC_Payroll_Summary]
 GO
