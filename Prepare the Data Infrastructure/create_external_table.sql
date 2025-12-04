d709cf6f9d204ea4ba768ae4a1809f55cbfb2bf9
Halah Abousohh <eng.halah.mohammed212@outlook.sa>
2025-12-04 08:15:31 +0100

    Update create_external_table.sql

--- Prepare the Data Infrastructure/create_external_table.sql
+++ Prepare the Data Infrastructure/create_external_table.sql
@@ -10,28 +10,24 @@ WITH (
 )
 GO
 
--- Create the External Data Source WITHOUT credential (for public access or managed identity)
-IF NOT EXISTS (SELECT * FROM sys.external_data_sources WHERE name = 'defs_de2_dfs_core_windows_net')
-CREATE EXTERNAL DATA SOURCE [defs_de2_dfs_core_windows_net]
+-- Create the External Data Source
+IF NOT EXISTS (SELECT * FROM sys.external_data_sources WHERE name = 'dirstaging_demostoarge22_dfs_core_windows_net')
+CREATE EXTERNAL DATA SOURCE [dirstaging_demostoarge22_dfs_core_windows_net]
 WITH (
-    LOCATION = 'abfss://defs@de2.dfs.core.windows.net'
+    LOCATION = 'abfss://dirstaging@demostoarge22.dfs.core.windows.net'
     -- No credential needed if using public access or Synapse workspace managed identity
 )
 GO
 
 -- Create the External Table
-CREATE EXTERNAL TABLE [dbo].[NYC_Payroll_Summary](
+CREATE EXTERNAL TABLE [dbo].[NYC_Payroll_Summary2](
     [FiscalYear] [int] NULL,
     [AgencyName] [varchar](50) NULL,
     [TotalPaid] [float] NULL
 )
 WITH (
-    LOCATION = '/',
-    DATA_SOURCE = [defs_de2_dfs_core_windows_net],
+    LOCATION = '/dirstaging',
+    DATA_SOURCE = [dirstaging_demostoarge22_dfs_core_windows_net],
     FILE_FORMAT = [SynapseDelimitedTextFormat]
 )
-GO
-
--- Check if it was created successfully
-SELECT TOP 10 * FROM [dbo].[NYC_Payroll_Summary]
 GO
