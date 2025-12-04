800928a56b117cdd68bc350a8c28761b24a747b3
Halah Abousohh <eng.halah.mohammed212@outlook.sa>
2025-12-04 15:56:11 +0100

    Revise external table and data source definitions
    Updated external file format and data source for new container. Modified external table structure and location.

--- Prepare the Data Infrastructure/create_external_table.sql
+++ Prepare the Data Infrastructure/create_external_table.sql
@@ -1,33 +1,30 @@
--- Create the External File Format
 IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name = 'SynapseDelimitedTextFormat') 
-CREATE EXTERNAL FILE FORMAT [SynapseDelimitedTextFormat] 
-WITH ( 
-    FORMAT_TYPE = DELIMITEDTEXT,
-    FORMAT_OPTIONS (
-        FIELD_TERMINATOR = ',',
-        USE_TYPE_DEFAULT = FALSE
-    )
-)
+	CREATE EXTERNAL FILE FORMAT [SynapseDelimitedTextFormat] 
+	WITH ( FORMAT_TYPE = DELIMITEDTEXT ,
+	       FORMAT_OPTIONS (
+			 FIELD_TERMINATOR = ',',
+			 FIRST_ROW = 2,  -- Skips header row
+			 USE_TYPE_DEFAULT = FALSE
+			))
 GO
 
--- Create the External Data Source
-IF NOT EXISTS (SELECT * FROM sys.external_data_sources WHERE name = 'dirstaging_demostoarge22_dfs_core_windows_net')
-CREATE EXTERNAL DATA SOURCE [dirstaging_demostoarge22_dfs_core_windows_net]
-WITH (
-    LOCATION = 'abfss://dirstaging@demostoarge22.dfs.core.windows.net'
-    -- No credential needed if using public access or Synapse workspace managed identity
-)
+-- Updated: container22 instead of dirstaging
+IF NOT EXISTS (SELECT * FROM sys.external_data_sources WHERE name = 'container22_demostoarge22_dfs_core_windows_net') 
+	CREATE EXTERNAL DATA SOURCE [container22_demostoarge22_dfs_core_windows_net] 
+	WITH (
+		-- Changed: container22 instead of dirstaging
+		LOCATION = 'abfss://container22@demostoarge22.dfs.core.windows.net' 
+	)
 GO
 
--- Create the External Table
-CREATE EXTERNAL TABLE [dbo].[NYC_Payroll_Summary2](
-    [FiscalYear] [int] NULL,
-    [AgencyName] [varchar](50) NULL,
-    [TotalPaid] [float] NULL
-)
-WITH (
-    LOCATION = '/dirstaging',
-    DATA_SOURCE = [dirstaging_demostoarge22_dfs_core_windows_net],
-    FILE_FORMAT = [SynapseDelimitedTextFormat]
-)
+CREATE EXTERNAL TABLE [dbo].[NYC_Payroll_Summary] (
+	[AgencyName] nvarchar(4000),
+	[FiscalYear] bigint,
+	[TotalPaid] float
+	)
+	WITH (
+	LOCATION = '/',  -- Root of container22
+	DATA_SOURCE = [container22_demostoarge22_dfs_core_windows_net],  -- Updated
+	FILE_FORMAT = [SynapseDelimitedTextFormat]
+	)
 GO
